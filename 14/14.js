399876245c4b1401205caec4b17b93cdba6f3b69
Erika Francesconi <fiorediren@gmail.com>
2020-12-20 12:23:00 +0100

    Day 14 Part 2

--- 14/14.js
+++ 14/14.js
@@ -41,7 +41,7 @@ fs.readFile('./input.txt', (err, data) => {
     return result;
   }
 
-  const mem = {};
+  let mem = {};
   fullInstructions.forEach(set => {
     const { mask, instructions } = set;
     instructions.forEach(instruction => {
@@ -57,6 +57,63 @@ fs.readFile('./input.txt', (err, data) => {
     tot += toDecimal(mem[idx]);
   }
 
-  console.log(tot);
+  console.log('Result 1', tot);
+
+  const applyMaskAddress = (num, mask) => {
+    num = num.padStart(mask.length, '0');
+    const arrMask = mask.split('');
+    const result = num.split('').reduce((acc, val, idx) => {
+      acc += arrMask[idx] === '0' ? val : (arrMask[idx] === 'X' ? 'X' : '1');
+      return acc;
+    }, '');
+    return result;
+  }
+
+  const reduceAddresses = address => {
+    const baseNumber = toDecimal(address.replaceAll('X', '0'));
+
+    const reversed = address.split('').reverse();
+    const valuesToAdd = reversed.reduce((acc, val, idx) => {
+      if (val === 'X') {
+        const pow = Math.pow(2, idx);
+        if (acc.length > 0) {
+          acc.forEach(val => {
+            acc.push(val + pow);
+          })
+        }
+        acc.push(pow);
+      }
+      return acc;
+    }, []);
+    
+    // console.log(valuesToAdd);
+
+    const allAddresses = valuesToAdd.map(val => toBinary(baseNumber + val).padStart(address.length, '0'));
+    allAddresses.push(toBinary(baseNumber).padStart(address.length, '0'));
+    // console.log(allAddresses);
+    return allAddresses;
+
+  }
+
+  mem = {};
+  fullInstructions.forEach(set => {
+    const { mask, instructions } = set;
+    instructions.forEach(instruction => {
+      const memId = instruction.substring(instruction.indexOf('[') + 1, instruction.indexOf(']'));
+      const val = instruction.substring(instruction.indexOf('=') + 2);
+      const address = applyMaskAddress(toBinary(memId), mask);
+      reduceAddresses(address).forEach(val1 => {
+        mem[val1] = toBinary(+val);
+      });
+    })
+  });
+  // console.log(mem);
+  
+  tot = 0;
+  Object.keys(mem).forEach(idx => {
+    tot += toDecimal(mem[idx]);
+  })
+
+  console.log('Result 2', tot);
 
 });
